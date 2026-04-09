fc3d4eee41219e432d06cde1bbd210ac1222f479
Adnan Doric <464180+astronaute77@users.noreply.github.com>
2026-04-09 12:46:11 +0400

    Add version to startup log

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import { loadConfig } from "./config.js";
 import { createLogger } from "./logger.js";
 import { buildApp } from "./app.js";
 
+declare const __VERSION__: string;
+
 const config = loadConfig(process.env);
 const logger = createLogger();
 const server = buildApp(config, logger);
 
 server.listen(config.PORT, "0.0.0.0", () => {
-  logger.info({ port: config.PORT }, "plexscan started");
+  logger.info({ version: __VERSION__, port: config.PORT }, "plexscan started");
 });
